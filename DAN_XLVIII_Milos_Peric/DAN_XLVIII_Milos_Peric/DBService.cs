d2a8534b652090f25d2aa8a633b80ccc8836ce61
Milos Peric <steamestt@gmail.com>
2020-08-03 14:48:58 +0200

    +Add Database Model

---
+++ DAN_XLVIII_Milos_Peric/DAN_XLVIII_Milos_Peric/DBService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAN_XLVIII_Milos_Peric
+{
+    class DBService
+    {
+        public List<tblPizza> GetPizzas()
+        {
+            try
+            {
+                using (PizzeriaServiceEntities context = new PizzeriaServiceEntities())
+                {
+                    List<tblPizza> list = new List<tblPizza>();
+                    list = (from p in context.tblPizzas select p).ToList();
+                    return list;
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception" + ex.Message.ToString());
+                return null;
+            }
+        }
+
+        public tblOrder AddOrder(tblOrder order)
+        {
+            try
+            {
+                using (PizzeriaServiceEntities context = new PizzeriaServiceEntities())
+                {
+                    tblOrder newOrder = new tblOrder();
+                    newOrder.CustomerJMBG = order.CustomerJMBG;
+                    newOrder.OrderStatus = order.OrderStatus;
+                    newOrder.OrderDateTime = order.OrderDateTime;
+                    context.tblOrders.Add(newOrder);
+                    context.SaveChanges();
+                    order.ID = newOrder.ID;
+                    return order;
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception" + ex.Message.ToString());
+                return null;
+            }
+        }
+
+        public tblPizzaOrder AddPizzaOrder(tblPizzaOrder pizzaOrder)
+        {
+            try
+            {
+                using (PizzeriaServiceEntities context = new PizzeriaServiceEntities())
+                {
+                    tblPizzaOrder newPizzaOrder = new tblPizzaOrder();
+                    newPizzaOrder.Amount = pizzaOrder.Amount;
+                    newPizzaOrder.OrderID = pizzaOrder.OrderID;
+                    newPizzaOrder.PizzaID = pizzaOrder.PizzaID;
+                    context.tblPizzaOrders.Add(newPizzaOrder);
+                    context.SaveChanges();
+                    pizzaOrder.ID = newPizzaOrder.ID;
+                    return pizzaOrder;
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception" + ex.Message.ToString());
+                return null;
+            }
+        }
+    }
+}
---
+++ DAN_XLVIII_Milos_Peric/DAN_XLVIII_Milos_Peric/View/ViewLogin.xaml.cs
@@ -0,0 +1,29 @@
+﻿using DAN_XLVIII_Milos_Peric.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DAN_XLVIII_Milos_Peric.View
+{
+    /// <summary>
+    /// Interaction logic for ViewLogin.xaml
+    /// </summary>
+    public partial class ViewLogin : Window
+    {
+        public ViewLogin()
+        {
+            InitializeComponent();
+            DataContext = new LoginViewModel(this);
+        }
+    }
+}
